f3f0c80525cc6e657f59283bdedbafc6d1de5d49
A20B08365 educacion <A20B08365@educacion.edu.do>
2023-01-15 19:56:34 -0500

    Versión 1.2

--- Trabajo grupal/Inventario.cs
+++ Trabajo grupal/Inventario.cs
@@ -16,5 +16,10 @@ namespace Trabajo_grupal
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

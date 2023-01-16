0aab333944fee9d43c91b55319ad30e9e2949441
A20B08365 educacion <A20B08365@educacion.edu.do>
2023-01-15 20:09:34 -0500

    Version 1.5

--- Trabajo grupal/Facturacion.Designer.cs
+++ Trabajo grupal/Facturacion.Designer.cs
@@ -50,7 +50,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
@@ -97,7 +96,6 @@
             // 
             this.panel5.BackColor = System.Drawing.Color.LightGray;
             this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel5.Controls.Add(this.button1);
             this.panel5.Controls.Add(this.dataGridView1);
             this.panel5.Controls.Add(this.textBox2);
             this.panel5.Controls.Add(this.textBox1);
@@ -270,15 +268,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(290, 77);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 96);
-            this.button1.TabIndex = 17;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Facturacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -323,6 +312,5 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
     }
 }

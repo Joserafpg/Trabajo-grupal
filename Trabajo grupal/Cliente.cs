526e5746e86733979619df57158806a1c86dcc5e
King Sosa <antonio098x@gmail.com>
2023-01-22 20:47:56 -0400

    version 2.9

--- Trabajo grupal/Cliente.cs
+++ Trabajo grupal/Cliente.cs
@@ -16,5 +16,15 @@ namespace Trabajo_grupal
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

9d774e3757998ab5f659857b5b32f6fc346a76f6
Joserafpg <payerogarciaj@gmail.com>
2023-04-26 18:59:19 -0400

    Version 6.3.2

---
+++ Trabajo grupal/Conexion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo_grupal
+{
+    public class Conexion
+    {
+        private static SqlConnection Conn = new SqlConnection("Data source = DESKTOP-CDKTRK2; Initial Catalog=Proyecto_Grupal; Integrated Security=True");
+
+        public static SqlConnection ObtenerConexion()
+        {
+            return Conn;
+        }
+
+        public static void opoencon()
+        {
+            try
+            {
+                Conn.Open();
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public static void cerrarcon()
+        {
+            if (Conn != null)
+            {
+                try
+                {
+                    Conn.Close();
+                }
+
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}

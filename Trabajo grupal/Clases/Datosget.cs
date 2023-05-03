e40bca55a5eb964f491cd692f1160ddca5ae653a
A20B08365 educacion <A20B08365@educacion.edu.do>
2023-05-03 11:13:40 -0400

    7.3

--- Trabajo grupal/Clases/Datosget.cs
+++ Trabajo grupal/Clases/Datosget.cs
@@ -37,6 +37,31 @@ namespace Trabajo_grupal.Clases
 
         }
 
+        public int Codigo { get; set; }
+        public string Nombre_Producto { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public int Inv_Minimo { get; set; }
+        public string Mercancia_Utilizada { get; set; }
+        public string Precio_Venta { get; set; }
+        public string Precio_PorMayor { get; set; }
+
+        public Datosget() { }
+
+        public Datosget(int pCodigo, string pNombre_Producto, string pDescripcion,   int pCantidad, int pInv_Minimo, string pMercancia_Utilizada, string Precio_Venta, string Precio_PorMayor)
+        {
+            this.Codigo = pCodigo;
+            this.Nombre_Mercancia = pNombre_Producto;
+            this.Descripcion = pDescripcion;
+            this.Cantidad = pCantidad;
+            this.Inv_Minimo = pInv_Minimo;
+            this.Mercancia_Utilizada = pMercancia_Utilizada;
+            this.Precio_Venta = Precio_Venta;
+            this.Precio_PorMayor = Precio_PorMayor;
+
+
+        }
+
 
     }
 }
